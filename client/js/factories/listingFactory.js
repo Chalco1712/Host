93f6abf2ed237d3165540cefed02f054b0bcdbba
Chalco1712 <32286464+Chalco1712@users.noreply.github.com>
2019-02-11 23:34:00 -0500

    f
    f

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,11 +1,11 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('http://agile-bootcamp4.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://agile-bootcamp4.herokuapp.com/api/listings', listing);
+	  return $http.post('http://agile-bootcamp4.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
